02841582637d873dba73b4fc655b8422d8dfe09c
Irene <204175078+irenesj87@users.noreply.github.com>
2025-11-28 13:06:06 +0100

    feature: add react-icons and update Logo component styling
    Added the 'react-icons' dependency and updated related package versions. Refactored the Logo component to use react-icons, updated its CSS module for improved styling, and made minor theme and TypeScript config adjustments.

--- excursiones/src/components/Logo/Logo.tsx
+++ excursiones/src/components/Logo/Logo.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { COMPANY_NAME, ROUTES } from "../../constants";
+import { PiMountainsFill } from "react-icons/pi";
 import styles from "./Logo.module.css";
 
 /**
  * Componente que renderiza el logo de la aplicación con el texto "Excursiones Juntos".
  */
-const Logo = (): React.ReactElement => {
+const Logo = (): JSX.Element => {
+	// Asignamos el icono a una constante con el tipo React.ElementType
+	// para asegurar a TypeScript que es un componente JSX válido.
+	const IconoMontanhas = PiMountainsFill as React.ComponentType<{
+		className: string;
+	}>;
+
 	return (
 		<Link to={ROUTES.HOME} className={styles.logoContainer}>
+			<IconoMontanhas className={styles.logoIcon} />
 			<span className={styles.logoText}>{COMPANY_NAME}</span>
 		</Link>
 	);
