f50963725af204b7306707b0e78a6d4d56993891
Irene <204175078+irenesj87@users.noreply.github.com>
2026-01-09 12:15:45 +0100

    refactor: update NoExcursionsFound component
    Improved accessibility and refactored messages in NoExcursionsFound component.

--- excursiones/src/components/ExcursionsList/NoExcursionsFound/NoExcursionsFound.tsx
+++ excursiones/src/components/ExcursionsList/NoExcursionsFound/NoExcursionsFound.tsx
@@ -2,21 +2,25 @@ import React from "react";
 import { SearchIcon } from "../../shared/Icons";
 import styles from "./NoExcursionsFound.module.css";
 
+const TITLE_MESSAGE = "No se encontraron excursiones con esas características.";
+const SUBTITLE_MESSAGE =
+	"Prueba a cambiar los filtros para refinar tu búsqueda.";
+
 /**
  * Muestra un mensaje cuando no se encuentran excursiones que coincidan con los criterios de búsqueda.
  */
 function NoExcursionsFound() {
 	return (
-		<div className={styles.excursionsContainer}>
-			<output className={styles.messageNotFound}>
-				<SearchIcon className={styles.messageIcon} />
-				<p className={styles.primaryMessage}>
-					No se encontraron excursiones con esas características.
-				</p>
-				<p className={styles.secondaryMessage}>
-					Prueba a cambiar los filtros para refinar tu búsqueda.
-				</p>
-			</output>
+		<div
+			className={styles.excursionsContainer}
+			role="status"
+			aria-live="polite"
+		>
+			<div className={styles.messageNotFound}>
+				<SearchIcon className={styles.messageIcon} aria-hidden="true" />
+				<p className={styles.primaryMessage}>{TITLE_MESSAGE}</p>
+				<p className={styles.secondaryMessage}>{SUBTITLE_MESSAGE}</p>
+			</div>
 		</div>
 	);
 }
